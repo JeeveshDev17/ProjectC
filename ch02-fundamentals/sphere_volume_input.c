8d1927e9d2251f5c7fc730635c583a43483c30a2
Jeevesh Dev <jeeveshdev17@gmail.com>
2026-05-18 02:36:46 +0530

    Ch2: volume of sphere with scanf for input

---
+++ ch02-fundamentals/sphere_volume_input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.1415926f
+int main(void)
+{
+    float radius, volume;
+    printf("Enter radius of sphere in cm = ");
+    scanf("%f", &radius);
+    volume = 4.0f/3.0f * PI * radius * radius * radius;
+    printf("Volume of sphere with radius %.2f cm = %.2f\n", radius, volume);
+    return 0;
+}
