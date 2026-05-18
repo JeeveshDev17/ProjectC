0235b1849810b24ee239893079f33356fa5e2baf
Jeevesh Dev <jeeveshdev17@gmail.com>
2026-05-18 10:18:59 +0530

    Ch2: polynomial function value using scanf

--- ch02-fundamentals/polynomial_function_calculator.c
+++ ch02-fundamentals/polynomial_function_calculator.c
@@ -8,4 +8,5 @@ int main(void){
     scanf("%d", &x);
     sol = x*x*x*x*x + 2*x*x*x*x - 5*x*x*x - x*x + 7*x - 6; 
     printf("The value of x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 is: %d\n", sol);
+    return 0;
 }
