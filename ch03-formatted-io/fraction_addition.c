1660519e5ac9340feff89204b1f7384247971f85
Jeevesh Dev <jeeveshdev17@gmail.com>
2026-05-22 13:00:00 +0530

    Ch3: two fraction adder

---
+++ ch03-formatted-io/fraction_addition.c
@@ -0,0 +1,28 @@
+
+/* KN King - Chapter 2: C Fundamentals
+   Program: Adding values of two fractions with User Input
+   Concepts: variables, scanf, printf, int arithmetic
+*/ 
+
+#include <stdio.h> 
+
+int main(void) 
+
+{ 
+
+int numl, denoml, num2, denom2, result_num, result_denom; 
+
+printf("Enter first fraction: "); 
+scanf("%d/%d", &numl, &denoml); 
+
+printf("Enter second fraction: "); 
+scanf("%d/%d", &num2, &denom2); 
+
+result_num = numl * denom2 + num2 * denoml; 
+
+result_denom = denoml * denom2;
+
+printf("The sum is %d/%d\n", result_num, result_denom); 
+return 0;
+
+}
