f9a82470170b0906f9e9a8f19f1fe9b6798bef70
Jeevesh Dev <jeeveshdev17@gmail.com>
2026-07-19 11:09:29 +0530

    Ch 3: DOB added if else

--- ch03-formatted-io/dob.c
+++ ch03-formatted-io/dob.c
@@ -9,7 +9,18 @@ int main(void)
 
   printf("Enter a date (mm/dd/yyyy): ");
   scanf("%d/%d/%d", &month, &day, &year);
-  printf("You entered the date %d%.2d%.2d\n", year, month, day);
+  if (month >= 1 && month <= 12)
+  {
+    
+    printf("You entered the date %d %.2d %.2d\n", year, month, day);
+    return 1;
+  
+  }
+
+  else 
+  {
+    printf("You entered an invalid date\n");
+  }
 
   return 0;
 }
