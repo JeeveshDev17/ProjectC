d5110ce9e6450f0a2c974dacc1be4422336f8094
Jeevesh Dev <jeeveshdev17@gmail.com>
2026-08-14 02:15:23 +0530

    My Lazy Version for UPC Check digit with minor fixes

--- ch04-expressions/upc_lazy.c
+++ ch04-expressions/upc_lazy.c
@@ -4,13 +4,13 @@
 #include<stdio.h>
 int main(void){
     int i, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, k;
-    printf("Enter The First (Single) Digit: ");
+    printf("Enter The First (Single) Digit: \n");
     scanf("%1d", &i);
-    printf("Enter The First Group of Five Digits: ");
+    printf("Enter The First Group of Five Digits: \n");
     scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
-    printf("Enter The Second Group of Five Digits: ");
+    printf("Enter The Second Group of Five Digits: \n");
     scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
     k = 9-(((3*(i+i2+i4+j1+j3+j5) + i1+i3+i5+j2+j4)-1) % 10);
-    printf("Check Digit: %1d", k);
+    printf("Check Digit: %d\n", k);
     return 0;
 }
